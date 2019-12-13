bf92e5dd2c8802702c72525037c31bed280e4e9a
gelafin <46856925+gelafin@users.noreply.github.com>
2019-12-13 11:20:37 -0800

    initial commit; pseudocode done

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdlib>
+#include <string>
+
+int main() {
+
+//ask how many players
+
+
+// ask what kind of composition
+// ask how long it will be
+// (later, ask if competitive)
+// notify player1 to start
+// cin to string composition.append() so it can be echoed in terminal
+// nextWritingSegmentLength() randomly returns "1 sentence" or "5 words" or
+//   "1 {line/stanza/paragraph}" to tell player how much he can write.
+//   (but it checks to make sure it's not asking for something beyond final length)
+//   save requirement to check next input for it
+// curveBall() throws in either
+//   "next letter must be x"
+//   "next word must be x"
+//   "next x words must start with x"
+//   "nth word must start with x"
+//   ""
+//   (but it checks to make sure it's not asking for something beyond final length)
+//   save requirement to check next input for it
+// print composition
+// player sees new rules and submits new string to append
+// notify player if he broke any rules and print composition again if he did,
+//   else increment number of lines and
+//   if still player1 time, run above game functions again.
+//   Elsif still playing, notify player2 to start
+//   Else game is done, show final composition
+//   (later, once competitive is possible, ask all players for comparison)
+
+return 0;
+}
